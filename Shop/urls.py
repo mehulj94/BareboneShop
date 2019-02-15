717e81da01e3de58b039f5e3655745201c5849eb
Mehul Jain <mehulj94@gmail.com>
2019-02-14 22:35:51 -0500

    GraphQL implementation

--- Shop/urls.py
+++ Shop/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Barebones.urls')),
